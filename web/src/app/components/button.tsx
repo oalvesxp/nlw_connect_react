49fe652e3d3e602606bae633509b545ac029f93b
Osvaldo Alves <oalves.neto@outlook.com>
2025-02-23 14:45:26 -0300

    feat: update button component

--- web/src/app/components/button.tsx
+++ web/src/app/components/button.tsx
@@ -1,11 +1,31 @@
+import { ReactNode } from "react"
+
 interface ButtonProps {
-  text?: string
+  children: ReactNode
 }
 
 export function Button(props: ButtonProps){ 
   return (
-    <button type='button' className="bg-violet-500 px-5 py-2 rounded-sm mr-2">
-      {props.text || 'Enviar'}
+    <button 
+      type='button' 
+      className="
+        flex
+        justify-between
+        items-center
+        px-5 
+        h-12
+        bg-gray-500
+        text-blue 
+        font-semibold 
+        rounded-xl 
+        w-full 
+        cursor-pointer
+        hover:bg-blue 
+        hover:text-gray-900
+        transition-colors
+        duration-300
+      ">
+      {props.children}
     </button>
   )
 }
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,12 +1,13 @@
+import { ArrowRight } from 'lucide-react'
 import { Button } from "./components/button";
 
 export default function Home() {
   return (
     <main>
-      <h1 className="text-4xl font-semibold font-heading">Title from this page</h1>
-      <div>Hello world!</div>
-      <Button text="Hello" />
-      <Button />
+      <Button>
+        Enviar
+        <ArrowRight />
+      </Button>
     </main>
   );
 }
