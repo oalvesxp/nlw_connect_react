e48d39b84301dc3746b916948cceb31c934ecc1c
Osvaldo Alves <oalves.neto@outlook.com>
2025-02-24 00:24:27 -0300

    feat: setup ranking to get data from API

--- web/src/app/invite/[slug]/ranking.tsx
+++ web/src/app/invite/[slug]/ranking.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image'
 
+import { getRanking } from '@/http/api'
 import cooper from '../../../assets/medal_cooper.svg'
 import gold from '../../../assets/medal_gold.svg'
 import silver from '../../../assets/medal_silver.svg'
 
-export function Ranking() {
+export async function Ranking() {
+  const { ranking } = await getRanking()
+
   return (
     <div className="w-full max-w-[440px] space-y-5">
       <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
@@ -12,38 +15,46 @@ export function Ranking() {
       </h2>
 
       <div className="space-y-4">
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">1º</span> | Jhon Armless
-          </span>
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            1030
-          </span>
+        {ranking.map((subscriber, index) => {
+          const position = index + 1
 
-          <Image src={gold} alt="gold" className="absolute top-0 right-8" />
-        </div>
+          return (
+            <div
+              key={subscriber.id}
+              className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3"
+            >
+              <span className="text-sm text-gray-300 leading-none">
+                <span className="font-semibold">{position}º</span> |{' '}
+                {subscriber.name}
+              </span>
+              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+                {subscriber.score}
+              </span>
 
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">2º</span> | Jhon Doe
-          </span>
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            756
-          </span>
-
-          <Image src={silver} alt="gold" className="absolute top-0 right-8" />
-        </div>
-
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">3º</span> | Jane Doe
-          </span>
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            532
-          </span>
-
-          <Image src={cooper} alt="gold" className="absolute top-0 right-8" />
-        </div>
+              {position === 1 && (
+                <Image
+                  src={gold}
+                  alt="gold"
+                  className="absolute top-0 right-8"
+                />
+              )}
+              {position === 2 && (
+                <Image
+                  src={silver}
+                  alt="silver"
+                  className="absolute top-0 right-8"
+                />
+              )}
+              {position === 3 && (
+                <Image
+                  src={cooper}
+                  alt="cooper"
+                  className="absolute top-0 right-8"
+                />
+              )}
+            </div>
+          )
+        })}
       </div>
     </div>
   )
