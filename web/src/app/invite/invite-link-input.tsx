cf1a554a4749733740d783c6d1771eb5a4d0e2d7
Osvaldo Alves <oalves.neto@outlook.com>
2025-02-23 22:24:06 -0300

    feat: add function to copy invite link

--- web/src/app/invite/invite-link-input.tsx
+++ web/src/app/invite/invite-link-input.tsx
@@ -1,21 +1,28 @@
-import { Copy, Link } from "lucide-react";
-import { IconButton } from "../components/icon-button";
-import { InputField, InputIcon, InputRoot } from "../components/input";
+'use client'
 
-export function InviteLinkInput() {
-	return (
-		<InputRoot>
-			<InputIcon>
-				<Link size={16} />
-			</InputIcon>
-			<InputField
-				readOnly
-				defaultValue="http://localhost:3000/invite/1403740366920033052"
-			/>
+import { Copy, Link } from 'lucide-react'
+import { IconButton } from '../components/icon-button'
+import { InputField, InputIcon, InputRoot } from '../components/input'
 
-			<IconButton className="-mr-2">
-				<Copy size={16} />
-			</IconButton>
-		</InputRoot>
-	);
+interface InviteLinkProps {
+  url: string
+}
+
+export function InviteLinkInput(props: InviteLinkProps) {
+  function copyInviteLink() {
+    navigator.clipboard.writeText(props.url)
+  }
+
+  return (
+    <InputRoot>
+      <InputIcon>
+        <Link size={16} />
+      </InputIcon>
+      <InputField readOnly defaultValue={props.url} />
+
+      <IconButton className="-mr-2" onClick={copyInviteLink}>
+        <Copy size={16} />
+      </IconButton>
+    </InputRoot>
+  )
 }
