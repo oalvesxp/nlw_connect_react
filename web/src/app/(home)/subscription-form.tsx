8d2a4c7899571d991a048898d6d35acb7c52d937
Osvaldo Alves <oalves.neto@outlook.com>
2025-02-23 22:56:42 -0300

    feat: setup react hook form on subscription page

--- web/src/app/(home)/subscription-form.tsx
+++ web/src/app/(home)/subscription-form.tsx
@@ -1,34 +1,79 @@
-import { ArrowRight, Mail, User } from "lucide-react";
-import { Button } from "../components/button";
-import { InputField, InputIcon, InputRoot } from "../components/input";
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { ArrowRight, Mail, User } from 'lucide-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { Button } from '../components/button'
+import { InputField, InputIcon, InputRoot } from '../components/input'
+
+const subscriptionSchema = z.object({
+  name: z.string().min(2, 'Digite seu nome completo'),
+  email: z.string().email('Digite um e-mail válido'),
+})
+
+type SubscriptionSchema = z.infer<typeof subscriptionSchema>
 
 export function SubscriptionForm() {
-	return (
-		<form className="bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 w-full md:max-w-[400px]">
-			<h2 className="font-heading font-semibold text-gray-200 text-xl">
-				Inscrição
-			</h2>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SubscriptionSchema>({
+    resolver: zodResolver(subscriptionSchema),
+  })
 
-			<div className="space-y-3">
-				<InputRoot>
-					<InputIcon>
-						<User size={20} />
-					</InputIcon>
-					<InputField type="text" placeholder="Nome completo" />
-				</InputRoot>
+  function onSubscribe(data: SubscriptionSchema) {
+    console.log(data)
+  }
 
-				<InputRoot>
-					<InputIcon>
-						<Mail size={20} />
-					</InputIcon>
-					<InputField type="email" placeholder="E-mail" />
-				</InputRoot>
+  return (
+    <form
+      onSubmit={handleSubmit(onSubscribe)}
+      className="bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 w-full md:max-w-[400px]"
+    >
+      <h2 className="font-heading font-semibold text-gray-200 text-xl">
+        Inscrição
+      </h2>
 
-				<Button type="submit">
-					Confirmar
-					<ArrowRight />
-				</Button>
-			</div>
-		</form>
-	);
+      <div className="space-y-3">
+        <div className="space-y-2">
+          <InputRoot>
+            <InputIcon>
+              <User size={20} />
+            </InputIcon>
+            <InputField
+              type="text"
+              placeholder="Nome completo"
+              {...register('name')}
+            />
+          </InputRoot>
+
+          {errors.name && <p className="text-danger">{errors.name.message}</p>}
+        </div>
+
+        <div className="space-y-2">
+          <InputRoot>
+            <InputIcon>
+              <Mail size={20} />
+            </InputIcon>
+            <InputField
+              type="email"
+              placeholder="E-mail"
+              {...register('email')}
+            />
+          </InputRoot>
+
+          {errors.email && (
+            <p className="text-danger">{errors.email.message}</p>
+          )}
+        </div>
+
+        <Button type="submit">
+          Confirmar
+          <ArrowRight />
+        </Button>
+      </div>
+    </form>
+  )
 }
